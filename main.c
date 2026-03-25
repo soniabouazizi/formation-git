89799077720092c1eb0dd5c5257394e40041c40e
sonia <soniabouazizi297@gmail.com>
2026-03-25 22:35:27 +0100

    Add function to sum two numbers

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+// Fonction pour additionner deux nombres
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     printf("Hello Git\n");
+
+    int x = 5;
+    int y = 7;
+    int result = add(x, y);
+
+    printf("La somme de %d et %d est %d\n", x, y, result);
     return 0;
 }
