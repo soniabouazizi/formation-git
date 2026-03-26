7393fd65580e6725e7e260e96b7ceb9cf330905b
sonia <soniabouazizi297@gmail.com>
2026-03-26 09:40:09 +0100

    Update code in dev branch

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    printf("Hello Git\n");
+    printf("Hello Git (dev branch)\n");
+
+    int result = add(3, 4);
+    printf("Result = %d\n", result);
+
     return 0;
 }
